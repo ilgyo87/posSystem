fe4a49e420a5b3ab4bf9f1fe8d0c2b9fa0a95c48
Iggy <ilgyo87@gmail.com>
2025-03-01 19:57:12 -0500

    blank template again

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,43 +1,11 @@
 import { defineAuth } from "@aws-amplify/backend"
 
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend"
-
-const schema = a.schema({
-  Business: a
-    .model({
-      businessName: a.string(),
-      ownerEmail: a.string()
-    })
-    .authorization(allow => [allow.owner()]),
-
-  User: a
-    .model({
-      email: a.string(),
-      role: a.string()
-    })
-    .authorization(allow => [allow.owner()]),
-
-  Customer: a
-    .model({
-      name: a.string(),
-      phoneNumber: a.string()
-    })
-    .authorization(allow => [allow.owner()]),
-
-  Inventory: a
-    .model({
-      itemName: a.string(),
-      quantity: a.string(),
-      price: a.string()
-    })
-    .authorization(allow => [allow.owner()])
-})
-
-export type Schema = ClientSchema<typeof schema>
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "userPool"
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true
   }
 })
